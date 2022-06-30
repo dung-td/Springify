55e4ecc28d8bd302e078bdb51a42ec786db4f882
Tong Duc Dung <tngcdng@gmail.com>
2022-06-30 17:49:57 +0700

    update

--- src/interfaces/server.js
+++ src/interfaces/server.js
@@ -1,2 +1,2 @@
-//export const server = "https://springifybe.herokuapp.com/api"
-export const server = "http://localhost:8080/api"
+export const server = "https://springifybe.herokuapp.com/api"
+// export const server = "http://localhost:8080/api"
