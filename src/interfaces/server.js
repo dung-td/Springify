a34d30f34b081ae5976a3a65d3898b044ad73824
Tong Duc Dung <tngcdng@gmail.com>
2023-10-18 20:47:23 +0700

    Change to AWS server

--- src/interfaces/server.js
+++ src/interfaces/server.js
@@ -1,3 +1,3 @@
 // export const server = "https://springifybe.herokuapp.com/api"
 // export const server = "http://localhost:8080/api"
-export const server = "http://springify.ap-southeast-2.elasticbeanstalk.com/api/"
+export const server = "http://springify.ap-southeast-2.elasticbeanstalk.com/api"
