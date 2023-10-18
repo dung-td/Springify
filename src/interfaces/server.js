872a0145641d6851e377dfa341d86d4d8cf51dcf
Tong Duc Dung <tngcdng@gmail.com>
2023-10-18 23:18:45 +0700

    Change server to Render.com

--- src/interfaces/server.js
+++ src/interfaces/server.js
@@ -1,3 +1,4 @@
 // export const server = "https://springifybe.herokuapp.com/api"
 // export const server = "http://localhost:8080/api"
-export const server = "http://springify.ap-southeast-2.elasticbeanstalk.com/api"
+// export const server = "http://springify.ap-southeast-2.elasticbeanstalk.com/api"
+export const server = "https://springify-service.onrender.com/api"
