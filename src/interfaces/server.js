2fea37175ba49e79328311abcd48d3f437b30da7
Tong Duc Dung <tngcdng@gmail.com>
2023-10-21 09:15:06 +0700

    Update server.js

--- src/interfaces/server.js
+++ src/interfaces/server.js
@@ -2,4 +2,4 @@
 // export const server = "http://localhost:8080/api"
 // export const server = "http://springify.ap-southeast-2.elasticbeanstalk.com/api"
 // export const server = "https://springify-service.onrender.com/api"
-export const server = "https://springify-elb-1720100636.ap-southeast-2.elb.amazonaws.com/api"
+export const server = "https:api-springify.noter.id.vn/api"
